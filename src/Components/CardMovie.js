c8fc3c4cbce5bfa043a860f656fb3bd4e4ce0fa5
mzaouchi <mzaouchi@gmail.com>
2024-07-30 12:39:25 +0100

    Second

--- src/Components/CardMovie.js
+++ src/Components/CardMovie.js
@@ -1,6 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import Rating from '@mui/material/Rating';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const CardMovie=({el})=>{
     const [readMore, setReadMore] = useState(false);
@@ -9,7 +10,7 @@ const CardMovie=({el})=>{
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={el.posterURL} />
       <Card.Body>
-        <Card.Title>{el.title}</Card.Title>
+        <Card.Title style={{textDecoration :"none", color :"black", fontWeight:'bold'}} as={Link} to={`/MovieDetails/${el.id}`}>{el.title}</Card.Title>
         <Card.Text>
         {readMore ? el.description: `${el.description.substring(0, 40)}...`}
         <button className={readMore ? "buttMoreRED" : "buttMoreGREEN"} onClick={()=> setReadMore(!readMore)}>{readMore ? "Show less" : "Show more"}</button>
--- src/Components/ListMovies.js
+++ src/Components/ListMovies.js
@@ -1,12 +1,22 @@
+import AddMovie from "./AddMovie"
 import CardMovie from "./CardMovie"
+import FilterMovie from "./FilterMovie"
 
-const ListMovies=({movies,textLive,rate})=>{
+const ListMovies=({movies,textLive,rate,setMovies,setRate,setTextLive})=>{
     var x = movies.filter((el,i,t)=> el.title.toUpperCase().includes(textLive.toUpperCase()) && el.rating >= rate)
     return(
-        <div className="listMovies">
-            {
-              x.length == 0 ? <h1>Not Found</h1> :  x.map((el,i,t)=> <CardMovie el={el}/>)
-            }
+        <div>
+            <div className='filterAdd'>
+                <FilterMovie textLive={textLive} rate={rate} setTextLive={setTextLive} setRate={setRate}/>
+                <AddMovie setMovies={setMovies} movies={movies}/>
+            </div>
+
+            <div className="listMovies">
+                {
+                x.length == 0 ? <h1>Not Found</h1> :  x.map((el,i,t)=> <CardMovie el={el}/>)
+                }
+            </div>
+           
         </div>
     )
 }
